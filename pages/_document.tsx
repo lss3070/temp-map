eb782f982f46717950e71fbc02507370e801f92a
이송섭 <isongseob@songseop.local>
2022-11-22 10:22:59 +0900

    Update meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     return (
       <Html>
         <Head >
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
         <Script strategy="beforeInteractive"
          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=046f31cfae67c4eb66f86f01861abf28&libraries=services,clusterer&autoload=false`}
         />
