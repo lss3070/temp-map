10fa1252de5085c3593b218c4dac11491ca8d99b
이송섭 <isongseob@songseop.local>
2022-12-08 17:21:11 +0900

    #20 scroll on top event 추가

--- components/restaurant/restaurantList/LeftRestaurantList.tsx
+++ components/restaurant/restaurantList/LeftRestaurantList.tsx
@@ -1,15 +1,16 @@
-import { faLocationDot, faMarker, faStar, faWon } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot, faMarker, faSortUp, faStar, faWon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
 import { stat } from "fs";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useMyPositionStore } from "../../../store/myPosition.store";
 import { useRoadStore } from "../../../store/road.store"
 import { getDistance } from "../../../utils/getDistance";
 
 const LeftRestaurantList=()=>{
 
+    const areaRef=useRef<HTMLDivElement>(null)
     const myPosition=useMyPositionStore((state)=>state.myPosition)
     const [
         onRoad,
@@ -47,14 +48,24 @@ const LeftRestaurantList=()=>{
         show:{opacity: 1, translateX: '0%'}
     }
 
+    const scrollTop=()=>{
+       areaRef.current?.scrollTo({top:0,behavior:'smooth'})
+    }
     return(
         <>
         {
-                <div className="grid py-5 bg-gray-300 overflow-y-scroll z-10">
+                <div 
+                ref={areaRef}
+                className="grid py-5 bg-gray-300 overflow-y-scroll z-10 relative">
+                    <div className="flex w-full border">
+                        <div className="w-full text-center">이미지</div>
+                        <div className="w-full text-center">게시판</div>
+                    </div>
+                    <div>
+                        <div className="w-full flex justify-end">정렬기준</div>
+                    </div>
                     {
                         restuarantMark?.map((restaurant,index)=>{
-
-                            const distance= getDistance(myPosition?.lat!,myPosition?.lng!,restaurant.lat,restaurant.lng)
                             return(
                                 <div key={index} 
                                 onClick={()=>setSelectMark(restaurant)}
@@ -73,7 +84,7 @@ const LeftRestaurantList=()=>{
                                             />
                                             <div className="absolute top-2 left-2 z-40 bg-gray-800 bg-opacity-60 text-white px-2 py-1 rounded-xl flex gap-1 items-center">
                                                 <FontAwesomeIcon icon={faLocationDot}/>
-                                                {distance}m
+                                                {restaurant.distance}m
                                             </div>
                                             <div className="absolute bottom-0 right-0 z-40 bg-gray-800 bg-opacity-60 text-white px-2 rounded-tl-xl flex gap-1">
                                                 <a>
@@ -95,6 +106,13 @@ const LeftRestaurantList=()=>{
                             )
                         })
                     }
+                    <div className="fixed right-2 bottom-2 rounded-full w-9 h-9  bg-gray-500 flex
+                    items-center justify-center cursor-pointer z-50 text-white
+                    " 
+                    onClick={scrollTop}
+                    >
+                        <FontAwesomeIcon size={'1x'} icon={faSortUp}/>
+                    </div>
                 </div>
         }
         </>
